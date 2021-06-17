3ced1f116f744bb800e07ff071ad58727ceef16c
Charissa <cjrequena827@gmail.com>
2021-06-17 19:01:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+
+//const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+function addingEventListener(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert)
+}
+input.addEventListener('click', clickAlert); 
+
+    
+    
+    
+    
+
